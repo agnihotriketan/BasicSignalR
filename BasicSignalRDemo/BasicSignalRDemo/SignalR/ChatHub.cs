4dd356408a678099cf915956af6a3b41612bd843
unknown <kagnihotri@Crux.internal.tavisca.com>
2015-12-23 16:24:46 +0530

    third

--- BasicSignalRDemo/BasicSignalRDemo/SignalR/ChatHub.cs
+++ BasicSignalRDemo/BasicSignalRDemo/SignalR/ChatHub.cs
@@ -6,12 +6,17 @@ using Microsoft.AspNet.SignalR;
 
 namespace BasicSignalRDemo.SignalR
 {
-    public class ChatHub:Hub
+    public class ChatHub : Hub
     {
+        /*
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
+        }*/
+        public void Send(string name, string msg)
+        {
+            Clients.All.broadcastMessage(name, msg);
         }
     }
 }
